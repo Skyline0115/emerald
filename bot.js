9f3c6cf0ea025a06ef1b612dd061081fd090e93d
Skyline0115 <41397893+Skyline0115@users.noreply.github.com>
2018-07-19 01:58:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,7 @@ client.on('ready', () => {
 });
 
 
-client.on("message", async message => {
-	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
-	 if(command === "ping") {
+client.on("message", message => {
 	 	message.channel.send('pong!');
 	 }
 }
