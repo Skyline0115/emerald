6aa7700c7df0ae208c802ebe0ccf69f20e5465e8
Skyline0115 <41397893+Skyline0115@users.noreply.github.com>
2018-07-19 01:20:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,35 @@ client.on('ready', () => {
 });
 
 
-
-
+client.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+ if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+        	case 'ping':
+                	client.sendMessage({
+                    	to: channelID,
+                    	message: 'Pong!'
+                	});
+            	break;
+			
+		case 'Hello':
+			client.sendMessage({
+			to: channelID,
+			message: 'Hello, Master'
+			});
+		break;
+            // Just add any case commands if you want to..
+         }
+     }
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
+
+
