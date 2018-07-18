2485f9540f36921cca8f2fa92fb614b7e17f0c12
Skyline0115 <41397893+Skyline0115@users.noreply.github.com>
2018-07-19 01:17:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,17 +5,9 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
-});
 
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
+
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
